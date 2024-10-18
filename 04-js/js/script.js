c900789d689ca6a362ff063e793e63a927c800a3
odd:odd <eehd80yul@gmail.com>
2024-10-18 13:19:40 +0900

    jquery

--- 04-js/js/script.js
+++ 04-js/js/script.js
@@ -26,15 +26,8 @@ console.log(a);
 const b = 3;
 console.log(b); // 3
 
-b = 4;
+// b = 4;
 console.log(b); // Uncaught TypeError: Assignment to constant variable.
-
-// 함수 선언
-function 함수명() {
-  // 실행될 코드
-}
-// 함수 호출
-함수명();
 
 // 객체 선언
 const obj = {
@@ -47,3 +40,37 @@ const obj = {
 // 산술 연산자
 console.log(1 + 2); // 3
 console.log(6 % 6); // 0
+
+// 조건문
+
+// if (조건) {
+//   // 조건이 참일 때 실행될 코드
+// } else {
+//   // 조건이 거짓일 때 실행될 코드
+// }
+
+let c = 2;
+if (c === 1) {
+  console.log("참");
+} else {
+  console.log("거짓");
+}
+
+// 함수 선언
+function 함수명() {
+  // 실행될 코드
+}
+// 함수 호출
+함수명();
+
+// 이벤트
+// 도큐먼트가 준비가 되면 실행
+
+$(document).ready(function () {
+  document.querySelector("button").addEventListener("click", function () {
+    console.log("클릭");
+
+    // .box를 숨기기
+    $(".box").hide();
+  });
+});
