5eafc254786a1a09cfe3c31fba5a5e4f63788d7d
odd:odd <eehd80yul@gmail.com>
2024-10-22 12:10:46 +0900

    트리거

--- 04-js/js/gsap.js
+++ 04-js/js/gsap.js
@@ -1,9 +1,22 @@
-// gsap.to("누구를", { 어떻게 });
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.fromTo(".green", { x: 200, y: -100, opacity: 0 }, { x: 0, opacity: 1, duration: 1 });
-gsap.to(".green", { y: 0, duration: 1 }, 1);
-gsap.from(".orange", { x: -200, opacity: 0, duration: 1 }, 2);
-gsap.from(".yellow", { x: 200, opacity: 0, duration: 1 }, 1);
-gsap.to(".box-area", { rotation: 90, duration: 1 }, 3);
-gsap.to(".green, .yellow, .orange", { borderRadius: "50%", duration: 1 }, 4);
-gsap.from(".intro .txt", { scale: 0, opacity: 0, duration: 1 }, 5);
+var tl = gsap.timeline();
+
+// gsap.to("누구를", { 어떻게 });
+tl.fromTo(".green", { x: 200, y: -100, opacity: 0 }, { x: 0, opacity: 1, duration: 1 }).to(".green", { y: 0, duration: 1 }).from(".orange", { x: -200, opacity: 0, duration: 1 }).from(".yellow", { x: 200, opacity: 0, duration: 1 }).to(".box-area", { rotation: 90, duration: 1 }).to(".green, .yellow, .orange", { borderRadius: "50%", duration: 1 }).from(".intro .txt", { scale: 0, opacity: 0, duration: 1 });
+
+gsap.to(".grow", {
+  scrollTrigger: ".grow",
+  width: "70%",
+  height: "70%",
+  fontSize: "3rem",
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".sec-grow",
+    pin: true,
+    start: "top 0",
+    end: "+=500",
+    scrub: 1,
+    markers: true,
+  },
+});
