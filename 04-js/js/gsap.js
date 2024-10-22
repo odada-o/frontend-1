5e01534633e5a69c5439533d900e0596f338255d
odd:odd <eehd80yul@gmail.com>
2024-10-22 11:31:26 +0900

    gsap

--- 04-js/js/gsap.js
+++ 04-js/js/gsap.js
@@ -1,6 +1,9 @@
 // gsap.to("누구를", { 어떻게 });
-// gsap.to(".green", { left: "50%", transform: "translateX(-50%)" });
-// gsap.to(".green", { y: 100 }, 1);
 
 gsap.fromTo(".green", { x: 200, y: -100, opacity: 0 }, { x: 0, opacity: 1, duration: 1 });
 gsap.to(".green", { y: 0, duration: 1 }, 1);
+gsap.from(".orange", { x: -200, opacity: 0, duration: 1 }, 2);
+gsap.from(".yellow", { x: 200, opacity: 0, duration: 1 }, 1);
+gsap.to(".box-area", { rotation: 90, duration: 1 }, 3);
+gsap.to(".green, .yellow, .orange", { borderRadius: "50%", duration: 1 }, 4);
+gsap.from(".intro .txt", { scale: 0, opacity: 0, duration: 1 }, 5);
