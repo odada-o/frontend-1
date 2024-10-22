e2dabb91cbba7c31a9cd789a022109ff75ecca8c
odd:odd <eehd80yul@gmail.com>
2024-10-22 12:22:43 +0900

    타임라인 트리거

--- 04-js/js/gsap.js
+++ 04-js/js/gsap.js
@@ -1,6 +1,15 @@
 gsap.registerPlugin(ScrollTrigger);
 
-var tl = gsap.timeline();
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".intro",
+    pin: true,
+    start: "top 0",
+    end: "+=500",
+    scrub: 1,
+    markers: true,
+  },
+});
 
 // gsap.to("누구를", { 어떻게 });
 tl.fromTo(".green", { x: 200, y: -100, opacity: 0 }, { x: 0, opacity: 1, duration: 1 }).to(".green", { y: 0, duration: 1 }).from(".orange", { x: -200, opacity: 0, duration: 1 }).from(".yellow", { x: 200, opacity: 0, duration: 1 }).to(".box-area", { rotation: 90, duration: 1 }).to(".green, .yellow, .orange", { borderRadius: "50%", duration: 1 }).from(".intro .txt", { scale: 0, opacity: 0, duration: 1 });
@@ -17,6 +26,6 @@ gsap.to(".grow", {
     start: "top 0",
     end: "+=500",
     scrub: 1,
-    markers: true,
+    // markers: true,
   },
 });
