0a67329da53b5f9ae446922a889dc263031c03a6
odd:odd <eehd80yul@gmail.com>
2024-12-11 10:24:33 +0900

    dd

--- 04-js/js/09-비동기.js
+++ 04-js/js/09-비동기.js
@@ -1,85 +1,146 @@
-const a = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("a 비동기");
-      resolve();
-    }, 1000);
-  });
-}
+// const a = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("a 비동기");
+//       resolve();
+//     }, 1000);
+//   });
+// }
 
-const b = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("b 비동기");
-      resolve(); // 성공 시 호출
-    }, 1000);
-  });
-};
+// const b = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("b 비동기");
+//       resolve(); // 성공 시 호출
+//     }, 1000);
+//   });
+// };
 
-const c = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("c 비동기");
-      resolve(); // 성공 시 호출
-    }, 1000);
-  });
-};
+// const c = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("c 비동기");
+//       resolve(); // 성공 시 호출
+//     }, 1000);
+//   });
+// };
 
-const d = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("d 비동기");
-      resolve(); // 성공 시 호출
-    }, 1000);
-  });
-};
+// const d = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("d 비동기");
+//       resolve(); // 성공 시 호출
+//     }, 1000);
+//   });
+// };
 
-Promise.all([a(), b(), c(), d()]).then(() => {
-  console.log("모두 완료");
-})
-
-// a()
-// .then(b)
-// .then(c)
-// .then(d)
-// .then(() => {
+// Promise.all([a(), b(), c(), d()]).then(() => {
 //   console.log("모두 완료");
 // })
 
-// a().then(() => {
-//   console.log("a함수 완료");
-//   b().then(() => {
-//     console.log("b함수 완료");
-//     c().then(() => {
-//       console.log("c함수 완료");
-//       d().then(() => {
-//         console.log("d함수 완료");
-//       })
+// // a()
+// // .then(b)
+// // .then(c)
+// // .then(d)
+// // .then(() => {
+// //   console.log("모두 완료");
+// // })
+
+// // a().then(() => {
+// //   console.log("a함수 완료");
+// //   b().then(() => {
+// //     console.log("b함수 완료");
+// //     c().then(() => {
+// //       console.log("c함수 완료");
+// //       d().then(() => {
+// //         console.log("d함수 완료");
+// //       })
+// //     })
+// //   })
+// // })
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//   const getUser = userId => {
+//     return new Promise((resolve, reject) => {
+//       fetch(`https://jsonplaceholder.typicode.com/posts/ddd/${userId}`)
+//         .then(res => {
+//           // 실패시 처리
+//           if (!res.ok) {
+//             throw new Error(`${userId}번 유저를 찾을 수 없습니다.`)
+//           }
+//           // 성공시 처리
+//           return res.json()
+//         })
+//         .then(data => resolve(data))
+//         .catch(error => reject(error))
 //     })
-//   })
-// })
+//   }
 
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+//   getUser(1)
+//     .then((data) => {
+//       console.log(data.title);
+//       return getUser(2);
+//     })
+//     .then((data) => {
+//       console.log(data.title);
+//       return getUser(3);
+//     })
+//     .then((data) => {
+//       console.log(data.title);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+//     // 무조건 실행
+//     .finally(() => {
+//       console.log("모두 완료");
+//     });
 
-  const getUser = userId => {
-    return new Promise((resolve) => {
-      fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
-        .then(res => res.json())
-        .then(data => resolve(data))
-    })
-  }
+//     // async await
+//     const printUser = async () => {
+//       try {
+//         const user1 = await getUser(1);
+//         console.log(user1.title);
 
-  getUser(1)
-    .then((data) => {
-      console.log(data.title);
-      return getUser(2);
-    })
-    .then((data) => {
-      console.log(data.title);
-      return getUser(3);
-    })
-    .then((data) => {
-      console.log(data.title);
-    });
+//         const user2 = await getUser(2);
+//         console.log(user2.title);
+
+//         const user3 = await getUser(3);
+//         console.log(user3.title);
+//       } catch(error) {
+//         console.log(error);
+//       } finally {
+//         console.log("모두 완료");
+//       }
+//     }
+
+//     printUser()
+
+
+
+//     // aa, bb 순서대로 출력
+//     const aa = () => {
+//       return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           console.log('aa')
+//           resolve()
+//         }, 1000)
+//       })
+//     }
+//     const bb = () => {console.log('bb')}
+
+//     aa().then(() => {return bb()})
+
+//     const main = async () => {
+//       await aa()
+//       bb()
+//     }
+    
+//     main()
+
+// console.log(fetch("https://jsonplaceholder.typicode.com/users"));
+
+fetch("https://jsonplaceholder.typicode.com/users").then((res) => console.log(res.json()));
---
+++ 06-ts/main.ts
@@ -0,0 +1,112 @@
+// typescript
+
+let hello: string = 'Hello World';
+
+hello = 'ddd';
+
+console.log(hello);
+
+
+// 구문
+
+// 변수명: 타입 = 값;
+const str: string = 'hello';
+const num: number = 10;
+const bool: boolean = true;
+const arr: number[] = [1, 2, 3];
+const obj: {name: string, age: number} = { name: 'kim', age: 20 };
+
+// 함수
+// function 함수이름(매개변수: 타입): 반환타입 {반환코드}
+// hello 함수 만들고 name 매개변수를 받아서 "안녕! 겨울" 반환
+function hello2(name: string): void {
+  console.log(name);
+}
+hello2('겨울');
+
+
+function hello3(name: string): string {
+  return name;
+}
+
+console.log(hello3('겨울'));
+
+const hello4 = function(age: number): number {
+  return age;
+}
+console.log(hello4(20))
+
+const hello5 = (name: string): string => {
+  return `안녕! ${name}`;
+}
+console.log(hello5('가을'));
+
+// interface
+interface User {
+  name: string;
+  age: number;
+}
+
+const user1: User = { name: 'kim', age: 20 };
+const user2: User = { name: "jeong", age: 10 };
+
+// 객체 타입
+const a: {name: string, age: number} = { name: 'kim', age: 20 };
+const b: { name: string, age: number } = { name: "jeong", age: 30 };
+
+// interface
+interface Ab {
+  name: string;
+  age: number;
+}
+
+const aa: Ab = { name: 'kim', age: 20 };
+const bb: Ab = { name: 'jeong', age: 30 };
+
+// 배열 타입
+const arr1: number[] = [1, 2, 3];
+const arr2: string[] = ['가', '나', '다'];
+const arr3: any[] = [1, '가', true];
+
+// 객체 배열 타입
+interface User {
+  name: string;
+  age: number;
+}
+const user: User[] = [
+  { name: "김가을", age: 2 },
+  { name: "김겨울", age: 3 },
+];
+
+// 객체 내부 객체 타입
+interface Address {
+  city: string;
+  dong: string;
+}
+
+interface User3 {
+  name: string;
+  age?: number;
+  address: Address;
+}
+
+const user3: User3 = {
+  name: "김봄",
+  address: {
+    city: "seoul",
+    dong: "gangnam",
+  },
+};
+
+// 일반 함수
+function sum (a: number, b: number) {
+  return a + b;
+}
+
+console.log(sum(1, 2))
+
+// 화살표 함수 타입
+const sum2: (a: number, b: number) => number = (a, b) => {
+  return a + b;
+}
+console.log(sum2(1, 2))
