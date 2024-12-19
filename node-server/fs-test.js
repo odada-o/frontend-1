7587f25840aeda0860d67e8fa9baab261f7a443d
odd:odd <eehd80yul@gmail.com>
2024-12-19 15:11:54 +0900

    query

--- node-server/fs-test.js
+++ node-server/fs-test.js
@@ -4,10 +4,19 @@ const fs = require("fs");
 const server = http.createServer(async (req, res) => {
   // 성공
   try {
-    // 파일 읽기
-    const data = await fs.promises.readFile('./index.html')
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data)
+    if(req.url === "/") {
+      // 파일 읽기
+      const data = await fs.promises.readFile('./index.html')
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data)
+    } else if(req.url === "/about") {
+      const data = await fs.promises.readFile("./about.html");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end('<h1>404 Not Found</h1>');
+    }
   }
   // 실패
   catch (error) {
---
+++ node-server/querystring.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring')
+
+// "http://localhost:8080/search?category=shoes&color=black&size=260";
+
+const server = http.createServer((req, res) => {
+  // url 문자열을 url 객체로 변환
+  const parsedUrl = url.parse(req.url)
+  // console.log(parsedUrl.query);
+  // 위에서 만든 객체 중 query 부분을 querystring 모듈로 객체로 변환
+  const query = querystring.parse(parsedUrl.query);
+  // query = { category: 'shoes', color: 'black', size: '260' }
+
+  // 상품 검색 처리
+  if (query.category || query.color) {
+    const result = `
+      <h2>검색결과</h2>
+      <p>카테고리: ${query.category}</p>
+      <p>카테고리: ${query.color}</p>
+    `;
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(result);
+  }
+})
+
+server.listen(8080, () => {
+  console.log('8080 서버 실행 중');
+})
