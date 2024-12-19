4f35b92b310f135fe709f2826ac720a7f2944d4c
odd:odd <eehd80yul@gmail.com>
2024-12-19 12:41:33 +0900

    fs-test

--- node-server/fs-test.js
+++ node-server/fs-test.js
@@ -1,8 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<p>노드 공부 중...</p>");
+const server = http.createServer(async (req, res) => {
+  // 성공
+  try {
+    // 파일 읽기
+    const data = await fs.promises.readFile('./index.html')
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data)
+  }
+  // 실패
+  catch (error) {
+    res.writeHead(500, { "Content-Type": "text/html" });
+    res.end(error.message);
+  }
 });
 
 server.listen(8080, () => {
